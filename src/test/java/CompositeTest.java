92a91010f690761f8936657ce08d79c3c172812b
Djordje Krstic <krsticdjordje372@gmail.com>
2025-07-15 15:12:24 +0200

    Created an interface Expression and a class Constant throughout the test method (that is, using TDD) that compares testValue and value inside variable 'e'.

--- src/test/java/CompositeTest.java
+++ src/test/java/CompositeTest.java
@@ -1,8 +1,16 @@
+import org.example.Constant;
+import org.example.Expression;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CompositeTest {
 
     @Test
-    public void testConstant() {}
+    public void testConstant() {
+        double testValue = 3.0;
+        Expression e = new Constant(testValue);
+        assertEquals(testValue, e.getValue(), 0.0);
+    }
 
 }
